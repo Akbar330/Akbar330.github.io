6b905642ab4554af60d1a5b3dffa73353341fbba
Akbar330 <nizarmuammad03@gmail.com>
2024-05-10 21:24:32 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Akbar330.github.io
---
+++ offline.js
@@ -0,0 +1,21 @@
+﻿{
+	"version": 1715347446,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/background-sheet0.png",
+		"images/bird-sheet0.png",
+		"images/pipebottom-sheet0.png",
+		"images/pipetop-sheet0.png",
+		"images/base.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
